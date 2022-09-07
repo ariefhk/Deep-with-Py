7a2d6ea1cdf3e95ca7268965b13d32fa93a8353c
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-09-07 20:38:42 +0700

    add py terbilang

--- Learn/2_tipe_data_&_variable/tipe_data.py
+++ Learn/2_tipe_data_&_variable/tipe_data.py
@@ -1,5 +1,18 @@
-print('hello world')
+
+satuan = ['', 'satu', 'dua', 'tiga', 'empat',
+              'lima', 'enam', 'tujuh', 'delapan', 'sembilan']
 
 
-def name():
-  print('test')
+def angkaTerbilang(angka):
+    if(angka < 0):
+        return 'negatif' + angkaTerbilang(-1)
+    elif(angka < 10):
+        return satuan[angka]
+    elif(angka < 100):
+        return satuan[int((angka-(angka % 10))/10)] + ' puluh ' + angkaTerbilang(angka % 10)
+    elif(angka < 200):
+        return satuan[int((angka - (angka % 100))/100)] + ' ratus ' + angkaTerbilang(angka % 100)
+
+
+print(angkaTerbilang(120))
+# print((120 - (120 % 100))/100)
