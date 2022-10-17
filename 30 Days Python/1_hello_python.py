753a6e3ac5f20c8ac935b2fb896838330e7fe7b1
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-10-17 23:18:48 +0700

    firts commit with 30 days

---
+++ 30 Days Python/1_hello_python.py
@@ -0,0 +1,29 @@
+# 1. Check Python Version
+import sys
+print(f'current python version: {sys.version}')
+
+# 2. Operation
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# 3. String
+name = 'Arief Rachman Hakim'
+country = 'Indonesia'
+sentences = 'I am enjoying 30 days of python'
+
+print(f'my name is {name}')
+print(f'i live in {country}')
+print(sentences)
+
+# 4. Check Data types
+print(type(10))
+print(type(9.8))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Arief Rachman Hakim'))
+print(type('Indonesia'))
